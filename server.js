dae0054a304a3c1d99ef0d35fd63a97f479a0b4c
eduardo222444 <1067786@senacminas.edu.br>
2025-07-21 15:43:40 -0300

    UPDATE

--- server.js
+++ server.js
@@ -1,20 +1,28 @@
-// Importa a instância do Sequelize configurada e o modelo de usuário
 const sequelize = require('./config/db');
 require('dotenv').config();
 const express = require('express')
+const cors = require('cors');
  
 const userRoutes = require('./routes/userRoutes')
 const contactRoutes = require('./routes/contactRoutes')
 const authRoutes = require('./routes/authRoutes')
+ 
 const app = express();
+ 
+app.use(cors({
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
  
 app.use(express.json());
  
-app.get('/', (_req, res)=> res.send('api funcionando'))
+app.get('/', (req, res)=> res.send('api funcionando'))
  
 app.use('/api/users', userRoutes)
-app.use('/api/contacts', contactRoutes)
+app.use('/api/contact', contactRoutes)
 app.use('/api/auth', authRoutes)
+ 
 const PORT = process.env.PORT;
  
 sequelize.authenticate()
@@ -26,3 +34,4 @@ sequelize.authenticate()
     console.log('banco de dados sincronizado')
     app.listen(PORT, () => console.log("SERVIDOR RODANDO NA PORTA: " + PORT))
   }).catch(erro => console.log("Erro interno do servidor", erro))
+ 
