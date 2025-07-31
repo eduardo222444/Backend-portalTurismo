9f9b41a7df1f188a85ea3ca14e71bd37edd8f419
eduardo222444 <1067786@senacminas.edu.br>
2025-07-31 14:42:17 -0300

    UPDATE

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,20 +1,20 @@
-const bcrypt =- require('bcryptjs');
+const bcrypt = require('bcryptjs');
 const User = require('../models/users');
-
  
  
-exports.login = async(req, res) =>{
+exports.login = async(req, res) => {
     try {
-        const {email,password} = req.body;
-        if(!email || !password) return res.status(400).json({mesage: 'email e senha são obrigatorios'})
+        const {email, password} = req.body;
+        if(!email || !password) return res.status(400).json({message: 'email e senha são obrigatorios'})
         const user = await User.findOne({where: {email}})
-        if(!user) return res.status(400).json({message: 'Usuario não encontrado'})
-        const passwordValid = await bcrypt.compare(password , user.password)
-        if(!passwordValid) return res.status(400).json({mesage: 'email e senha estão incorretos'})
+        if(!user) return res.status(404).json({message: 'Usuário não encontrado'})
+        const passwordValid = await bcrypt.compare(password, user.password)
+        if(!passwordValid) return res.status(400).json({message: 'email ou senha estão incorretos'})
         res.json({message: 'Login realizado com sucesso',
-        user : {id: user.id, name : user.name, email: user.email}
-        })
-     } catch (error) {
-        res.status(500).json({message: 'Erro interno do servidor' });
+                user: {id: user.id, name: user.name, email: user.email}
+            })
+    } catch (error) {
+        res.status(500).json({ message: 'Erro interno do servidor.', error });
     }
-} 
+}
+ 
--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = express.Router();
+const router = express.router();
 const contact = require('../controllers/contactController');
  
 // Criar um novo contato
